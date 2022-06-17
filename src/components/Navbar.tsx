5f44f5e21d6eac6c87c5f743ec7074de8362efcd
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2022-06-17 09:18:52 -0400

    refactor: defer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="bg-white py-6 flex justify-center rounded-b-2xl w-full">
       <div className="flex justify-between w-11/12 max-w-screen-lg items-center px-4">
-        <img src={Logo} alt="logo" className="h-6" />
+        <img src={Logo} alt="logo" className="h-6 w-28" />
         {/* <div className="flex gap-3 items-center">
           <img
             src={Photo}
